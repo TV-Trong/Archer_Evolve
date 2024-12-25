52a5c7b12105fda7464ad38ef2085c0f43484f13
TV-Trong <tvt180602@gmail.com>
2024-12-25 23:09:58 +0700

    Level up Attribute system part 1

---
+++ Archer Evolve/Assets/Scripts/Skill/AttributeManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AttributeManager : MonoBehaviour
+{
+    public List<LevelUpAttribute> attributes;
+    private void Awake()
+    {
+        LoadAttribute();
+    }
+    private void LoadAttribute()
+    {
+        string path = @"Assets/Scripts/Skill/Attributes.txt";
+        string json = File.ReadAllText(path);
+        Debug.Log(json);
+        attributes = JsonUtility.FromJson<AttributeWrapper>($"{{\"attributes\":{json}}}").attributes;
+    }
+}
+
+[System.Serializable]
+public class AttributeWrapper
+{
+    public List<LevelUpAttribute> attributes;
+}
+[System.Serializable]
+public class LevelUpAttribute
+{
+    public string name;
+    public string description;
+    public AttributeType type;
+    public float value;
+    public int rarity;
+}
+
+public enum AttributeType
+{
+    Strength,
+    MovementSpeed,
+    AttackSpeed,
+    Health,
+    Defense,
+    Regeneration,
+    CriticalChance,
+    CriticalDamage
+}
